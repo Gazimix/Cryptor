b867e962da011b29c53afba7aa68b1c581fe0b95
Gazimix <chendetails@gmail.com>
2022-08-12 22:15:44 +0300

    Formatted files

--- cryptor/Decryptor.java
+++ cryptor/Decryptor.java
@@ -39,9 +39,12 @@ public class Decryptor<T extends Serializable> extends Cryptor {
     }
 
     /**
-     * Call this function with a path to a file to decrypt the bytes from a file of a given object <T>
+     * Call this function with a path to a file to decrypt the bytes from a file of
+     * a given object <T>
      * that was encrypted previously to that file.
-     * It is imperative that the type we decrypt is the same type we encrypted earlier. 
+     * It is imperative that the type we decrypt is the same type we encrypted
+     * earlier.
+     * 
      * @param pathToFile path to file in which to save encrypted object
      */
     public T decrypt(String pathToFile) {
@@ -51,22 +54,22 @@ public class Decryptor<T extends Serializable> extends Cryptor {
         Path pth = Paths.get(pathToFile);
         File file = new File(pathToFile);
         try (FileInputStream fis = new FileInputStream(file);
-             CipherInputStream cis = new CipherInputStream(fis, cipher)) {
+                CipherInputStream cis = new CipherInputStream(fis, cipher)) {
             int fileSize = (int) Files.size(pth);
             long fileSizeWithPadding = fileSize + BUFFER_PAD;
 
-            if (fileSizeWithPadding >= Integer.MAX_VALUE){
+            if (fileSizeWithPadding >= Integer.MAX_VALUE) {
                 System.err.println(Cryptor.INPUT_FILE_TOO_BIG);
                 return null;
             }
             bytesData = new byte[(int) fileSizeWithPadding]; // allocate bytes array
             int cur = 0;
-            while ((bytesRead = cis.read(bytesData, cur, READ_SIZE)) != -1){
+            while ((bytesRead = cis.read(bytesData, cur, READ_SIZE)) != -1) {
                 cur += bytesRead;
             }
-                try (ByteArrayInputStream bis = new ByteArrayInputStream(bytesData);
-                     ObjectInputStream in = new ObjectInputStream(bis)) {
-                    retObj = (T) in.readObject();
+            try (ByteArrayInputStream bis = new ByteArrayInputStream(bytesData);
+                    ObjectInputStream in = new ObjectInputStream(bis)) {
+                retObj = (T) in.readObject();
             }
         } catch (IOException e) {
             System.err.println(Cryptor.BAD_IO_MSG);
